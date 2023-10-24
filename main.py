eb2074e67ee170fd9e6ea4e3236b89cc81ef2d2a
hdgarcia <hdgarcia@bancolombia.com.co>
2023-10-24 18:10:26 -0500

    Update

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+app.title = "Mi app con Fast API"
+app.version = "0.0.1"
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 movies = [
    {
@@ -9,12 +23,16 @@ movies = [
       "year": 2009,
       "rating": 7.8,
       "category": "Acción"
+   },
+   {
+      "id": 2,
+      "title": "Avatar 2",
+      "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+      "year": 2010,
+      "rating": 8.8,
+      "category": "Acción"
    }
 ]
-
-app = FastAPI()
-app.title = "Mi app con Fast API"
-app.version = "0.0.1"
 
 @app.get('/', tags=['Home'])
 def message():
@@ -27,3 +45,39 @@ def message():
 @app.get('/movies', tags=['Movies'])
 def get_movies():
     return movies
+
+@app.get('/movies/{id}', tags=['Movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return {'El Id ingresado no corresponde con un Id existente.'}
+
+@app.get('/movies/', tags=['Movies'])
+def get_movies_by_category(category: str, year: int):
+    return list(filter(lambda item: item['category'] == category and
+                       item['year'] == year, movies))
+
+@app.post('/movies', tags=['Movies'])
+def create_movie(movie: Movie):
+    movies.append(movie)
+    return movies
+
+@app.put('/movies/{id}', tags=['Movies'])
+def update_movie(id: int, movie: Movie):
+    for item in movies:
+        if item["id"] == id:
+            item['title'] = movie.title
+            item['year'] = movie.year
+            item['overview'] = movie.overview
+            item['rating'] = movie.rating
+            item['category'] = movie.category
+    return item
+
+@app.delete('/movies/{id}', tags=['Movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return movies
+    return {'El Id ingresado no corresponde con un Id existente.'}
